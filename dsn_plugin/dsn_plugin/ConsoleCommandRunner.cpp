0709a67ebf03e14692e1619bec89b73b4d5ca236
SwimmingTiger <hu60.cn@gmail.com>
2018-06-24 04:02:33 +0800

    add command `press <key> <time>` (input simulation not work).

--- dsn_plugin/dsn_plugin/ConsoleCommandRunner.cpp
+++ dsn_plugin/dsn_plugin/ConsoleCommandRunner.cpp
@@ -4,9 +4,42 @@
 #include "skse64/GameTypes.h"
 #include "Log.h"
 
+#include <sstream>
+#include <algorithm>
+#include <map>
+
 static IMenu* consoleMenu = NULL;
 
+std::vector<std::string> splitParams(std::string s) {
+	for (size_t i = 0; i<s.size(); i++) {
+		// replace blank characters to space
+		if (s[i] == '\t' || s[i] == '\n' || s[i] == '\r' ||
+			s[i] == '\0' || s[i] == '\x0B') {
+			s[i] = ' ';
+		}
+	}
+
+	std::stringstream ss(s);
+	std::string item;
+	std::vector<std::string> tokens;
+	while (std::getline(ss, item, ' ')) {
+		if (item.empty()) {
+			continue;
+		}
+		tokens.push_back(item);
+	}
+	return tokens;
+}
+
+std::mutex ConsoleCommandRunner::customCmdQueueLock;
+HANDLE ConsoleCommandRunner::customCmdQueueSemaphore;
+std::queue<std::vector<std::string>> ConsoleCommandRunner::customCmdQueue;
+
 void ConsoleCommandRunner::RunCommand(std::string command) {
+
+	if (TryRunCustomCommand(command)) {
+		return;
+	}
 
 	if (!consoleMenu) {
 		Log::info("Trying to create Console menu");
@@ -37,5 +70,139 @@ void ConsoleCommandRunner::RunCommand(std::string command) {
 	else
 	{
 		Log::info("Unable to find Console menu");
+	}
+}
+
+bool ConsoleCommandRunner::TryRunCustomCommand(const std::string & command) {
+	std::vector<std::string> params = splitParams(command);
+	
+	if (params.empty()) {
+		return false;
+	}
+	
+	std::string action = params[0];
+	// to lower
+	for (size_t i = 0; i < action.size(); i++) {
+		if ('A' <= action[i] && action[i] <= 'Z') {
+			action[i] += 'a' - 'A';
+		}
+	}
+	Log::info(std::string("action: ") + action);
+
+	if (action == "press") {
+		// mutex is automatically released when scopeLock goes out of scope
+		std::lock_guard<std::mutex> scopeLock(customCmdQueueLock);
+
+		// customCmdQueueSemaphore++
+		if (ReleaseSemaphore(customCmdQueueSemaphore, 1, NULL)) {
+			customCmdQueue.push(params);
+		}
+
+		return true;
+	}
+
+	return false;
+}
+
+DWORD WINAPI ConsoleCommandRunner::RunCustomCommandThread(void* ctx) {
+	customCmdQueueSemaphore = CreateSemaphore(NULL, 0, kMaxCustomCmdQueueSize, NULL);
+
+	for (;;) {
+		// waiting for customCmdQueueSemaphore > 0, then customCmdQueueSemaphore--
+		WaitForSingleObject(customCmdQueueSemaphore, INFINITE);
+
+		// mutex is automatically released when scopeLock goes out of scope
+		std::lock_guard<std::mutex> scopeLock(customCmdQueueLock);
+
+		std::vector<std::string> params = customCmdQueue.front();
+		customCmdQueue.pop();
+
+		if (params[0] == "press") {
+			RunCustomCommandPress(params);
+		}
+	}
+}
+
+void ConsoleCommandRunner::Initialize() {
+	CreateThread(NULL, 0, ConsoleCommandRunner::RunCustomCommandThread, NULL, 0L, NULL);
+}
+
+void ConsoleCommandRunner::RunCustomCommandPress(const std::vector<std::string>& params) {
+	std::vector<int /*key*/> keyDown;
+	std::map<int /*time*/, int /*key*/> keyUp;
+
+	// command: press <key> <time> <key> <time> ...
+	//           [0]   [1]   [2]    [3]   [4]
+	for (size_t i = 1; i + 1 < params.size(); i += 2) {
+		const std::string &keyStr = params[i];
+		const std::string &timeStr = params[i + 1];
+		int key = 0;
+		int time = 0;
+
+		if (keyStr.empty()) {
+			continue;
+		}
+
+		if (keyStr.size() > 2 && keyStr[0] == '0' && (keyStr[1] == 'x' || keyStr[1] == 'X')) {
+			// key code hex
+			key = strtol(keyStr.substr(2).c_str(), NULL, 16);
+		}
+		else if ('0' <= keyStr[0] && keyStr[0] <= '9') {
+			// key code dec
+			key = strtol(keyStr.c_str(), NULL, 10);
+		}
+		else if ('A' <= keyStr[0] && keyStr[0] <= 'Z') {
+			// character
+			key = (int)keyStr[0];
+		}
+		else if ('a' <= keyStr[0] && keyStr[0] <= 'z') {
+			// lower character, to upper
+			key = (int)(keyStr[0] - ('a' - 'A'));
+		}
+		else {
+			continue;
+		}
+
+		time = strtol(timeStr.c_str(), NULL, 10);
+
+		if (time <= 0) {
+			continue;
+		}
+
+		keyDown.push_back(key);
+
+		// Map is used to sort by time.
+		// Avoiding map key conflicts.
+		// Although it changes the time, it is more convenient than sorting by myself.
+		while (keyUp.find(time) != keyUp.end()) {
+			time++;
+		}
+		keyUp[time] = key;
+
+		// send KEY_DOWN
+		for (auto itr = keyDown.begin(); itr != keyDown.end(); itr++) {
+			INPUT input;
+			ZeroMemory(&input, sizeof(input));
+
+			input.type = INPUT_KEYBOARD;
+			input.ki.wVk = *itr;
+			SendInput(1, &input, sizeof(INPUT));
+		}
+
+		// send KEY_UP
+		int totalSleepTime = 0;
+		for (auto itr = keyUp.begin(); itr != keyUp.end(); itr++) {
+			int sleepTime = (*itr).first - totalSleepTime;
+			Sleep(sleepTime);
+			totalSleepTime += sleepTime;
+
+			INPUT input;
+			ZeroMemory(&input, sizeof(input));
+
+			input.type = INPUT_KEYBOARD;
+			input.ki.wVk = (*itr).second;
+			input.ki.dwFlags = KEYEVENTF_KEYUP;
+			SendInput(1, &input, sizeof(INPUT));
+		}
 	}
 }
--- dsn_plugin/dsn_plugin/ConsoleCommandRunner.h
+++ dsn_plugin/dsn_plugin/ConsoleCommandRunner.h
@@ -2,12 +2,35 @@
 #include "common/IPrefix.h"
 #include "skse64/GameMenus.h"
 #include <string>
+#include <vector>
+#include <mutex>
+#include <queue>
+#include <Windows.h>
 
 class ConsoleCommandRunner
 {
 private:
+	static const int kMaxCustomCmdQueueSize = 1000;
+
+	static std::mutex customCmdQueueLock;
+	static HANDLE customCmdQueueSemaphore;
+	static std::queue<std::vector<std::string>> customCmdQueue;
 
 public:
 	static void RunCommand(std::string command);
+	static bool TryRunCustomCommand(const std::string &command);
+	static DWORD WINAPI RunCustomCommandThread(void* ctx);
+	static void Initialize();
+
+	// Add a new command: press <character or VirtualKeyCode> <millisecond>
+	// Windows Virtual-Key Codes: https://msdn.microsoft.com/en-us/library/dd375731(VS.85).aspx
+	//           Example: press    Z 1000
+	//                    press  127 1000
+	//                    press 0x58 1000
+	//               Press 4 keys at the same time:
+	//                    press   a 1000   127 500   z 1000   0x58 3000
+	// Description: Simulate pressing the specified key the specified milliseconds.
+	//              Used to cast skills or dragon shouts or do other actions.
+	static void RunCustomCommandPress(const std::vector<std::string> &params);
 };
 

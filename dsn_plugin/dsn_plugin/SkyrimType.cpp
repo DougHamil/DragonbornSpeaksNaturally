beca2e9be9ace79d8de1e64478fddc8ceaf40d8b
SwimmingTiger <hu60.cn@gmail.com>
2019-01-10 00:35:43 +0800

    Compatible with Skyrim Special Edition 1.5.62

--- dsn_plugin/dsn_plugin/SkyrimType.cpp
+++ dsn_plugin/dsn_plugin/SkyrimType.cpp
@@ -3,13 +3,13 @@
 SkyrimType g_SkyrimType(SE);
 
 UInt64 SKYRIM_VERSION[3] = {
-	0x0001000500350000, // SkyrimSE 1.5.53.0
+	0x00010005003e0000,  // SkyrimSE 1.5.53.0
 	0x00010004000F0000,  // SkyrimVR
-	0x0000000000000000  // SkyrimVR BETA
+	0x0000000000000000   // SkyrimVR BETA
 };
 
 std::string SKYRIM_VERSION_STR[3] = {
-	"1.5.53.0",
+	"1.5.62.0",
 	"1.4.15.0",
 	"0.0.0.0"
 };

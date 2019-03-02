4aaac747a713bea236cc214c5095663944d24a23
SwimmingTiger <hu60.cn@gmail.com>
2019-03-02 18:18:11 +0800

    fix typo

--- dsn_plugin/dsn_plugin/SkyrimType.cpp
+++ dsn_plugin/dsn_plugin/SkyrimType.cpp
@@ -3,7 +3,7 @@
 SkyrimType g_SkyrimType(SE);
 
 UInt64 SKYRIM_VERSION[3] = {
-	0x00010005003e0000,  // SkyrimSE 1.5.53.0
+	0x00010005003e0000,  // SkyrimSE
 	0x00010004000F0000,  // SkyrimVR
 	0x0000000000000000   // SkyrimVR BETA
 };
